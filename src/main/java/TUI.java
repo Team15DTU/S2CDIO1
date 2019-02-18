24a2bb135a25bcbf92b3d7fee68ee3caccf0abe3
aksina <saaruppoulsen@gmail.com>
2019-02-18 11:23:21 +0100

    TUI
    Mangler switch koden (den metode som de skal køre alt efter om man vil slette, adde, se eller redigere en bruger)

---
+++ src/main/java/TUI.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class TUI {
+
+    public static void TUI() {
+
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            MenuBesked();
+            int choice = scan.nextInt();
+            TheSwitch(choice);
+
+        }
+
+    }
+
+
+    public static void MenuBesked() {
+        System.out.println("Enter a number for witch action you want to take");
+        System.out.println("1. Add new user");
+        System.out.println("2. Show users");
+        System.out.println("3. Opdate user");
+        System.out.println("4. Remove user");
+    }
+
+    public static void TheSwitch(int choice) {
+
+        switch (choice) {
+            case 1: //some thing
+                System.out.println("du tastede 1, kode skal ind her");
+                break;
+            case 2: //some thing
+                System.out.println("du tastede 2, kode skal ind her");
+                break;
+            case 3: //some thing
+                System.out.println("du tastede 3, kode skal ind her");
+                break;
+            case 4: //some thing
+                System.out.println("du tastede 4, kode skal ind her");
+                break;
+            default:
+                System.out.println("Please enter a valid input");
+                System.out.println();
+        }
+
+    }
+
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,6 +5,9 @@ public class main {
 
     public static void main(String[] args) {
 
+        TUI tui = new TUI();
+        tui.TUI();
+
 
     }
 }
