8ac28007eae42549203885846045bf0af333efa4
rasmussl <43174424+rasmussl@users.noreply.github.com>
2019-02-18 11:34:27 +0100

    Merge branch 'dev' of C:\Users\Rasmus\Documents\GitHub\S2CDIO1 with conflicts.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,15 @@
+import dto.Roles_Enum;
+
 /**
  * @author Rasmus Sander Larsen
  */
 public class main {
 
     public static void main(String[] args) {
+
+        Roles_Enum role = Roles_Enum.Admin;
+
+        System.out.println(role);
 
     }
 }
