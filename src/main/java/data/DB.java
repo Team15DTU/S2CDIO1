840c2ebfcf7ad3f05d965a986819ccbfa859e63f
Anders <s185100@student.dtu.dk>
2019-02-20 01:36:39 +0100

    Opdaterede pom.xml så den bruger MySQL Connector driver. Lavede også nogle ændringer til hvordan vi tilslutter os vores database i programmet.

--- src/main/java/data/DB.java
+++ src/main/java/data/DB.java
@@ -9,23 +9,23 @@ import java.util.List;
 
 public class DB implements IUserDAO {
 	Conn c = new Conn();
-
 	
 	//region Public Methods
-	
+    //FIXME DALException virker ikke så den er udkommenteret i getUser for test testing.
 	@Override
-	public UserDTO getUser(int userId) throws DALException
+	public UserDTO getUser(int userId) //throws DALException
 	{
 		//TODO:
-		c.Connect("SELECT * FROM CDIO1 WHERE userId="+userId, "");
-
+		c.Connect("SELECT * FROM CDIO1 WHERE userId="+userId,"");
 		return null;
+
 	}
 	
 	@Override
 	public List<UserDTO> getUserList() throws DALException
 	{
 		//TODO:
+        c.Connect("SELECT userID, userName, ini, cpr, roles FROM CDIO1", "");
 		return null;
 	}
 	
@@ -34,6 +34,7 @@ public class DB implements IUserDAO {
 	{
 		//TODO:
 
+        c.Connect("", "");
 	}
 	
 	@Override
--- src/main/java/data/conn/Conn.java
+++ src/main/java/data/conn/Conn.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 
 public class Conn {
     public void Connect(String stmt, String stmt2){
-        String valg;
         Scanner scanner = new Scanner(System.in);
-        boolean forkert;
 
         //loadDriver(); //Obsolete - only needed in rare cases.
         //try with resources (Java 7) - automatically calls connection.close() on end of try-catch block
@@ -16,36 +14,20 @@ public class Conn {
                 "s185114","LJYIY16CjeCktHQSlYnXS")){
             Statement statement = connection.createStatement();
             //statement.executeQuery("use s185114");
-            do {
+           // do {
+
+                if (!stmt2.equals("")) {
+                    statement.executeUpdate(stmt2);
+                }
                 ResultSet resultSet = statement.executeQuery(stmt);
-                System.out.println("Got resultset from database:");
+
                 while (resultSet.next()){
                     System.out.println(resultSet.getString(1) + ": " + resultSet.getString(2));
                 }
-                System.out.println("Press 1 for at tilføje, 2 for at slette & 3 for at lukke");
 
-                do {
-                    valg = scanner.next();
-                    forkert = false;
-                    if (valg.equals("1")) {
-                        System.out.println("insert");
-                        int id = scanner.nextInt();
-                        String text = scanner.next();
-                        statement.executeUpdate("insert into test values (" + id + "," + "'" + text + "'" + ")");
-                    } else if (valg.equals("2")) {
-                        System.out.println("delete");
-                        int id = scanner.nextInt();
-                        statement.executeUpdate("DELETE FROM test where id=" + id);
-                    }  else if (!valg.equals("3")){
-                        System.out.println("indtast ny værdi");
-                        forkert=true;
-                    }
-                }while (forkert);
-
-            }while (!valg.equals("3"));
-            System.out.println("Hav en god dag.");
             scanner.close();
             statement.close();
+
         } catch (SQLException e) {
             //Remember to handle Exceptions gracefully! Connection might be Lost....
             e.printStackTrace();
