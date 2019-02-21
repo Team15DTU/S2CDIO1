7aaf9b352e606cbe0ce20aabc5cd1644ba7d0276
Anders <s185100@student.dtu.dk>
2019-02-21 21:51:22 +0100

    Updated test.

--- src/main/java/data/DB.java
+++ src/main/java/data/DB.java
@@ -11,12 +11,13 @@ public class DB implements IUserDAO {
 
 	private Connection createConnection() throws DALException {
 		try {
-			return DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s185114?",
-                    "s185114","LJYIY16CjeCktHQSlYnXS");
+			return DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s185100?",
+                    "s185100","MTY7Fw0BTWcnwMWmrHekT");
 		} catch (SQLException e) {
 			throw new DALException(e.getMessage());
 		}
 	}
+
     public void Test() throws DALException
     {
         Connection c = createConnection();
@@ -27,10 +28,20 @@ public class DB implements IUserDAO {
             Statement statement = c.createStatement();
 
             //statement.executeUpdate("SELECT * FROM CDIO1 WHERE userID=1");
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM CDIO1 WHERE userID=1");
+            ResultSet rs = statement.executeQuery("SELECT * FROM CDIO1 WHERE userID=1");
 
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1) + ": " + resultSet.getString(2));
+            while (rs.next()){
+				//FIXME skal implementeres ordentligt i klassen UserDTO
+            	int userId = rs.getInt("userID");
+				String userName = rs.getString("userName");
+				String ini = rs.getString("ini");
+				String cpr = rs.getString("cpr");
+				String password = rs.getString("password");
+				String roles = rs.getString("roles");
+
+				System.out.println(userId + "\t" + userName +
+                        "\t" + ini + "\t" + cpr +
+                        "\t\t" + password + "\t" +roles);
             }
 
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) throws IUserDAO.DALException {
         DB db = new DB();
         //TODO db.getUser gør ikke noget den står der bare for noget test. (kan fjernes)
-        db.getUser(1);
+        //db.getUser(1);
         db.Test();
     }
 }
