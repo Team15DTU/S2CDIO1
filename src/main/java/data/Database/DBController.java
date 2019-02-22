d6d66c0c7e87b00ac63e48111a7274d926f107e0
nikolajwassmann <nwassmann@hotmail.com>
2019-02-22 14:04:23 +0100

    Started on DBController

--- src/main/java/data/Database/DBController.java
+++ src/main/java/data/Database/DBController.java
@@ -1,4 +1,65 @@
 package data.Database;
 
+import TUI.TUI;
+import dal.UserDAO;
+import data.dto.UserDTO;
+
+/**
+ * @author Nikolaj Wassmann
+ */
+
 public class DBController {
+
+    /*
+    ----------------------------Fields-------------------------
+     */
+
+    UserDAO userDAO;
+    UserDTO userDTO;
+    TUI tui;
+
+
+
+    /*
+    --------------------------Contructor--------------------------
+     */
+
+
+    public DBController(){
+        userDAO = new UserDAO();
+        userDTO = new UserDTO();
+        tui = new TUI();
+    }
+
+
+
+    /*
+    --------------------------Public methods--------------------------
+     */
+
+    /**
+     * Vi skal have modificeret TUI så den æn
+     */
+
+    public void SQLDatabase (){
+        tui.TUI();
+
+    }
+
+    public void DTODatabase (){
+        tui.TUI();
+
+    }
+
+    public void HashmapDatabase () {
+
+    }
+
+
+
+    /*
+    --------------------------Private methods--------------------------
+     */
+
+
 }
