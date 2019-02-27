6771fde146887ce9cd9e51528a003ff4fb061d5c
Logstor <a.rottger_rydahl@live.dk>
2019-02-27 18:31:01 +0100

    * Corrected updateUser() in UserDAO.

--- src/main/java/data/dao/UserDAO.java
+++ src/main/java/data/dao/UserDAO.java
@@ -63,62 +63,22 @@ public class UserDAO implements IUserDAO {
 
     @Override
     public void updateUser(UserDTO user) throws DALException {
-
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Click 1 to enter, click 2 to skip");
-
-        // ID
-        System.out.println("Want to change User ID?");
-        int choice1 = scan.nextInt();
-        if (choice1 == 1) {
-            System.out.println("enter the new user ID");
-            int newUserID = scan.nextInt();
-            user.setUserId(newUserID);
+    
+        /*******************************
+         * Find the user that is changed,
+         * and update that element.
+         *******************************/
+        int userID = user.getUserId();
+        for (int i=0; i < userDTOList.size(); i++)
+        {
+            if (userDTOList.get(i).getUserId() == userID)
+            {
+                // Remove and add update UserDTO object
+                userDTOList.remove(i); userDTOList.add(user);
+                break;
+            }
+            
         }
-        scan.nextLine();
-
-        //User name
-        System.out.println("Want to change User Name?");
-        int choice2 = scan.nextInt();
-        scan.nextLine();
-        if (choice2 == 1) {
-            System.out.println("enter the new user name");
-            String newUserName = scan.nextLine();
-            user.setUserName(newUserName);
-        }
-
-
-        //User initials
-        System.out.println("Want to change initials?");
-        int choice3 = scan.nextInt();
-        scan.nextLine();
-        if (choice3 == 1) {
-            System.out.println("enter the new initials");
-            String newUserIni = scan.nextLine();
-            user.setIni(newUserIni);
-        }
-
-        //User CPR
-        System.out.println("Want to change CPR number?");
-        int choice4 = scan.nextInt();
-        scan.nextLine();
-        if (choice4 == 1) {
-            System.out.println("enter the new CPR number");
-            String newCPR = scan.nextLine();
-            user.setCpr(newCPR);
-        }
-
-        //User password
-        System.out.println("Want to change password?");
-        int choice5 = scan.nextInt();
-        scan.nextLine();
-        if (choice5 == 1) {
-            System.out.println("enter the new password");
-            String newPassword = scan.nextLine();
-            user.setPassword(newPassword);
-        }
-
     }
 
 
